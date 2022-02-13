59c6e9cb0bc0ff14bb40fd22a8727d3ab6dd7e58
TAMAGO2682 <99587022+TAMAGO2682@users.noreply.github.com>
2022-02-13 21:07:55 +0900

    Update Prime_Factorization.ps1

--- Prime_Factorization.ps1
+++ Prime_Factorization.ps1
@@ -1,22 +1,21 @@
-[bigint]$n = Read-Host "Input num"
+[bigint]$num = Read-Host "Input num"
 $start = Get-Date
-$pf = @()
-while(([bigint]::Divide($n,2)) -eq 0){
-    $pf += 2
-    $n/=2
-}
-[bigint]$f = 3
-while([Bigint]::Compare($f*$f,$n) -le 0){
-    if($n%$f -eq 0){
-        $pf += $f
-        $n/=$f
-    }else{
-        $f += 2
+$pf = [ordered]@{}
+[bigint]$prime = 2
+while(([bigint]::Compare($prime,$num)) -le 0){
+    [bigint]$expon = 0
+    [bigint]$reminder = 0
+    [void]([bigint]::DivRem($num,$prime,[ref]$reminder))
+    while($reminder -eq 0){
+        $expon += 1
+        $num /= $prime
+        [void]([bigint]::DivRem($num,$prime,[ref]$reminder))
     }
-}
-if($n -ne 1){
-    $pf += $n
+    if($expon -ne 0){
+        $pf[$prime] = $expon
+    }
+    $prime += 1
 }
 $time = (Get-Date) - $start
 Write-Host $time
-Write-Host $pf
+$pf
