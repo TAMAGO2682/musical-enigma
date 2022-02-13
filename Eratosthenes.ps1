89195f28498ea1f8a038042114ea9d41ee1d6d0c
TAMAGO2682 <99587022+TAMAGO2682@users.noreply.github.com>
2022-02-13 21:55:33 +0900

    Update Eratosthenes.ps1

--- Eratosthenes.ps1
+++ Eratosthenes.ps1
@@ -1,25 +1,42 @@
 Function Eratosthenes {
-    [bigint]$num = Read-Host "Input num`t"
+    param([bigint]$num)
     [bigint]$root = [Math]::Exp([BigInt]::Log($num) / 2)
-    [bigint[]]$list = [bigint[]]::new($num+1)
-    for($i=0; $i -le $num; $i+=1){$list[$i] = $i}
-    foreach($div2 in $list[3..($list.LongLength)]){
-        if($div2%2 -eq 0){$list[$div2] = 0}
-    }
-    foreach($prim in $list[3..($list.LongLength)]){
-        if($prim -gt $root){break}
-        if($list[$prim] -ne 0){
-            for($mult=$prim; $prim*$mult -le $num; $mult+=2){
-                $list[($prim*$mult)] = 0
-            }
+    [bigint]$amount = 1
+    $list = [Boolean[]]::new($num+1)
+    $list[2] = $true
+    for($count=3;$count -lt ($list.Longlength);$count+=1){
+        if($count%2 -ne 0){
+            $list[$count]=$true
+            $amount+=1
         }
     }
-    $primes = ($list | Where-Object {$_ -ne 0 -and $_ -ne 1})
-    return $primes
+    for($prime=3;$prime -le $list.Longlength;$prime+=1){
+        if($prime -gt $root){break}
+        if($list[$prime] -eq $true){
+            for($mult=$prime; $prime*$mult -le $num; $mult+=2){
+                $target = ($prime*$mult)
+                if($list[$target] -eq $true){
+                    $list[$target] = $false
+                    $amount -= 1
+                }
+            } 
+        }
+    }
+    #$trueのインデックスを取得し、$primesに格納する。ようにする。
+    [bigint]$next = 0
+    $primes = [bigint[]]::new($amount)
+    for([bigint]$store=0;$store -le $list.LongLength;$store+=1){
+        if($list[$store] -eq $true){
+            $primes[$next] = $store
+            $next += 1
+        }
+    }
+    return $amount,$primes#($list.where{$_ -eq $true}).Count
 }
 
+[bigint]$num = Read-Host "Input num`t"
 $start = Get-date
-$result = Eratosthenes
+$amount,$primes = Eratosthenes -num $num
+Write-Host "Total Primes`t:" $amount "`n" $primes
 $time = (Get-Date) - $start
-Write-Host "Total Primes`t:" $result.Count "`n" $result
 $time.ToString()
